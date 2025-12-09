e658826d9cf476ebcc30534a9b8f9ad4a388ea9e
_Prathap_ <144254323+PrathapDev03@users.noreply.github.com>
2025-12-09 15:32:51 +0530

    Update index and About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/prathap.jpg';
 
 const About = () => {
   return (
@@ -61,24 +60,6 @@ const About = () => {
             DOWNLOAD CV
           </a>
           
-        </div>
-        {/* Right Side */}
-        <div className="md:w-1/2 flex justify-center md:justify-end">
-          <Tilt
-            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
-            tiltMaxAngleX={20}
-            tiltMaxAngleY={20}
-            perspective={1000}
-            scale={1.05}
-            transitionSpeed={1000}
-            gyroscope={true}
-          >
-            <img
-              src={profileImage}
-              alt="Tarun Kaushik"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
-            />
-          </Tilt>
         </div>
       </div>
     </section>
